189aee5eaa91ff83c748fa39a7f7dc2cc38500ac
oljaskrut <oljaskrutf@gmail.com>
2023-09-01 14:31:00 +0600

    magni cum laude

---
+++ app/api/daily/route.ts
@@ -0,0 +1,42 @@
+import { formatTime, todayDate, windowDate } from "@/lib/dayjs"
+import { prisma } from "@/lib/prisma"
+import { NextResponse } from "next/server"
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const limit = +(searchParams.get("limit") || "10")
+  // const compact = Boolean(searchParams.get("compact")) || false
+
+  const data = await prisma.feed.findMany({
+    take: limit,
+    orderBy: {
+      length: "desc",
+    },
+    where: {
+      date: {
+        gte: todayDate(),
+      },
+      AND: {
+        isSummarized: true,
+      },
+    },
+    select: {
+      date: true,
+      length: true,
+      summary: true,
+      source: true,
+    },
+  })
+
+  const mod = data.map(
+    (item) =>
+      item.source +
+      "-(" +
+      formatTime(item.date) +
+      "):" +
+      item.summary +
+      ` (quoted ${item.length} times)`,
+  )
+
+  return NextResponse.json({ mod })
+}
